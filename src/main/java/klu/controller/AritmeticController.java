bdf53ed9de791e56bf4fd909d3c3f3526932c21e
Nandhu20-06 <nandithatadiparthi23@gmail.com>
2025-08-05 11:34:46 +0530

    v1

---
+++ src/main/java/klu/controller/AritmeticController.java
@@ -0,0 +1,20 @@
+package klu.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
+public class AritmeticController 
+{
+	@GetMapping("/add/{A}/{B}")
+	public String add(@PathVariable("A") int a, @PathVariable("B") int b)
+	{
+		return "Addition of two numbers = " + (a + b);
+	}
+
+}
